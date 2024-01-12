7dc82ad6f9fa4cea71d7a428e48c6c8b897f6fcd
giuliabaratella <giuliabaratella7@gmail.com>
2024-01-12 18:04:09 +0100

    updated project index /added buttons edit and delete

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,15 +3,64 @@
     <section class="container">
         <h1>Projects</h1>
 
-        <ul>
-            @foreach ($projects as $project)
-                <li>
-                    <a href="{{ route('admin.projects.show', $project->id) }}">
-                        {{ $project->title }}
-                    </a>
-                </li>
-            @endforeach
+        <div class="container">
+            @if (session()->has('message'))
+                <div class="alert alert-success">{{ session('message') }}</div>
+            @endif
+            <div class="card">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Link</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
 
-        </ul>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($projects as $project)
+                            <tr>
+                                <th>
+                                    <a href="{{ route('admin.projects.show', $project->id) }}">
+                                        {{ $project->title }}
+                                    </a>
+                                </th>
+                                <td>
+                                    <a href="{{ $project->link }}">
+                                        {{ $project->link }}
+                                    </a>
+                                </td>
+                                <td><a href="{{ route('admin.projects.edit', $project->id) }}">
+                                        <button class="btn btn-success rounded-3 border-0 mx-5">
+                                            <i class="fa-solid fa-pen" style="font-size: 0.7rem"></i>
+                                        </button>
+                                    </a>
+                                </td>
+
+                                <td>
+                                    <form action="{{ route('admin.projects.destroy', $project->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger rounded-3 border-0 mx-5">
+                                            <i class="fa-solid fa-trash" style="font-size: 0.7rem"></i>
+                                        </button>
+                                    </form>
+
+
+                                </td>
+
+                            </tr>
+                        @empty
+                            <div>No projects</div>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+        </div>
+
+
     </section>
 @endsection
